4b30dc523334c97b36c8a9bafb2f06342408fa96
yonas <folkcoreano@outlook.com>
2023-02-19 14:47:27 -0300

    injeção

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import {createApp} from "vue";
+import {createPinia} from "pinia";
+import {inject} from "@vercel/analytics";
+
 import App from "@/App.vue";
 import "virtual:windi.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
+
+inject();
 
 app.mount("#app");
